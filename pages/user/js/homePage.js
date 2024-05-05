571c6858d18f7049f3b2eba18e2e74faaca4f637
phannhuthao01 <hanguyen20405@gmail.com>
2024-05-05 21:07:47 +0700

    homepage

--- pages/user/js/homePage.js
+++ pages/user/js/homePage.js
@@ -79,6 +79,63 @@ function displayWishlistInfo(product) {
   wishlistItemsDiv.appendChild(productImage);
 }
 
+var myCarousel = document.querySelector('#carouselExampleIndicators');
+      var carousel = new bootstrap.Carousel(myCarousel, {
+        interval: 2000, // Thời gian chuyển đổi giữa các slide (miliseconds)
+        wrap: true // Cho phép quay lại slide đầu khi đã chuyển qua slide cuối cùng
+      });
+
+
+      document.addEventListener("DOMContentLoaded", function () {
+        const messageModal = document.getElementById("messageModal");
+        const messageForm = document.getElementById("messageForm");
+
+        // Hiển thị modal khi nhấn vào biểu tượng tin nhắn
+        const floatingIcon = document.querySelector(".floating-icon");
+        floatingIcon.addEventListener("click", function () {
+          const modal = new bootstrap.Modal(messageModal, {
+            keyboard: false,
+          });
+          modal.show();
+        });
+
+        // Xử lý khi người dùng gửi tin nhắn
+        messageForm.addEventListener("submit", function (event) {
+          event.preventDefault();
+          const name = document.getElementById("messageName").value;
+          const content = document.getElementById("messageContent").value;
+
+          // Ở đây bạn có thể thực hiện xử lý gửi tin nhắn, ví dụ:
+          // Gửi thông tin name và content đến server
+
+          // Sau khi xử lý, có thể hiển thị thông báo thành công hoặc đóng modal
+          alert("Message sent successfully!");
+          const modal = bootstrap.Modal.getInstance(messageModal);
+          modal.hide();
+
+          // Để tránh gửi form nhiều lần, bạn có thể reset form sau khi gửi
+          messageForm.reset();
+        });
+      });
+
+      // Hàm để thêm sản phẩm vào giỏ hàng
+      function addToCart(product) {
+        let cart = JSON.parse(localStorage.getItem('cart')) || []; // Lấy giỏ hàng từ localStorage
+        cart.push(product); // Thêm sản phẩm vào giỏ hàng
+        localStorage.setItem('cart', JSON.stringify(cart)); // Lưu giỏ hàng mới vào localStorage
+        updateCartCount(cart.length); // Cập nhật số lượng sản phẩm trong giỏ hàng trên biểu tượng giỏ hàng
+      }
+
+      // Hàm để cập nhật số lượng sản phẩm trên biểu tượng giỏ hàng
+      function updateCartCount(count) {
+        document.getElementById('cart_count').innerText = count;
+      }
+
+      // Hàm để chuyển hướng đến trang cart.html khi người dùng nhấn vào biểu tượng giỏ hàng
+      function goToCart() {
+        window.location.href = './cart.html';
+      }
+
 
 
 
